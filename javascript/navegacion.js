c82003d03bd6755193affc662ef129393cf6dff9
Brayan Crispin <brayan.crispin@outlook.com>
2024-03-15 08:59:24 -0500

    Modulo de usuarios y registro

--- javascript/navegacion.js
+++ javascript/navegacion.js
@@ -4,7 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "actas.html";
   }
 
+  function singin(event) {
+    event.preventDefault();
+    window.location.href = "singin.html";
+  }
+
   document.getElementById("btnLogin").addEventListener("click", login);
+  document.getElementById("btnSingin").addEventListener("click", singin);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -28,10 +34,16 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "compromisos.html";
   }
 
+  function usuarios(event) {
+    event.preventDefault();
+    window.location.href = "usuarios.html"
+  }
+
   document.getElementById("btnLogout").addEventListener("click", logout);
   document.getElementById("btnActa").addEventListener("click", acta);
   document.getElementById("btnProyecto").addEventListener("click", proyecto);
   document.getElementById("btnCompromiso").addEventListener("click", compromiso);
+  document.getElementById("btnUsuarios").addEventListener("click",usuarios);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -50,3 +62,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.getElementById("nueva-acta").addEventListener("click",actaNueva);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  function login(event) {
+    event.preventDefault();
+    window.location.href = "login.html";
+  }
+
+  document.getElementById("btnCrearUsuario").addEventListener("click",login);
+  document.getElementById("btnVolverLogin").addEventListener("click",login);
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   function name(params) {
+    
+//   }  
+// });
